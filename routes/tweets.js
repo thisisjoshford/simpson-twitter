e6c59eabf97be6a987458b99003ce4fd24ba169f
Josh Ford <thisisjoshford@gmail.com>
2020-03-25 14:39:16 -0700

    update tweet by id test passing!

--- routes/tweets.js
+++ routes/tweets.js
@@ -15,11 +15,25 @@ module.exports = Router()
       .catch(next);
   })
 
+  .get('/:id', (req, res, next) => {
+    Tweet
+      .findById(req.params.id)
+      .then(tweet => res.send(tweet))
+      .catch(next);
+  })
+
   .get('/', (req, res, next) => {
     Tweet
       .find()
       .then(tweets => res.send(tweets))
       .catch(next);
   
+  })
+  
+  .patch('/:id', (req, res, next) => {
+    Tweet
+      .findByIdAndUpdate(req.params.id, { text: req.body.text }, { new: true })
+      .then(tweet => res.send(tweet))
+      .catch(next);
   });
 
